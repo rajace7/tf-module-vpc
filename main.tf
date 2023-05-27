fd4735f7e72f52f28c7260ddeb3572e50aaa23bd
2rpadala <rajvja520002@gmail.com>
2023-05-27 19:12:22 +0530

    vpc peering

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ resource "aws_internet_gateway" "igw" {
 
 resource "aws_eip" "ngw" {
   count = length(var.subnets["public"].cidr_block)
-  vpc   = true
+  //vpc   = true
   tags  = merge(var.tags, { Name = "${var.env}-ngw" })
 }
 
@@ -65,4 +65,23 @@ resource "aws_route" "ngw" {
   route_table_id         = local.all_private_subnet_ids[count.index]
   nat_gateway_id         = element(aws_nat_gateway.ngw.*.id, count.index)
   destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_vpc_peering_connection" "peer" {
+  peer_vpc_id = var.default_vpc_id
+  vpc_id      = aws_vpc.main.id
+  auto_accept = true
+}
+
+resource "aws_route" "peering_connection_route" {
+  count                     = length(local.all_private_subnet_ids)
+  route_table_id            = element(local.all_private_subnet_ids, count.index)
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+  destination_cidr_block    = var.default_vpc_cidr
+}
+
+resource "aws_route" "peering_connection_route_in_default_vpc" {
+  route_table_id            = var.default_vpc_rtid
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+  destination_cidr_block    = var.cidr_block
 }
